6fb2bbcff8fd7a61b7f5a7fb3a7df75bf5f3bbd9
Dat Vu <redb1997@gmail.com>
2019-11-18 16:21:17 +0300

    [egg_info]

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const deepCopy = (input) => {
+    const output = Array.isArray(input) ? [] : {};
+
+    for (const key in input) {
+        const value = input[key];
+        output[key] = (typeof value === "object") ? deepCopy(value) : value;
+    }
+
+    return output;
+}
